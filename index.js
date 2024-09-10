d79506a7b6c8b7f87e5d1da82f9dcafc07a0c676
samaraby <samara.monteiro.miranda@escola.pr.gov.br>
2024-09-10 01:26:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const primeiroNome = "Samara";
+const sobreNome = "Byanca";
+const idade = 17;
+const cidadeNatal = "ilha comprida sp";
+const altura = 1.70;
+
+console.log("o meu primeiro nome é:");
+console.log(primeiroNome);
+console.log("o sobre nome é:");
+console.log(sobreNome);
+console.log("eu tenho");
+console.log(idade);
+console.log("eu morava em:");
+console.log(cidadeNatal);
+console.log("minha altura é:");
+console.log(altura);
